60491bde80823ec4ad179baa7ef6e649ae9b1e0e
Ahmed Ragab <ahmedragab20901@gmail.com>
2025-01-21 02:47:01 +0200

    refactor: rename project and update stylesheet references

--- src/app.rs
+++ src/app.rs
@@ -16,7 +16,7 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <AutoReload options=options.clone() />
                 <HydrationScripts options/>
-                <link rel="stylesheet" id="leptos" href="/pkg/axum-leptos-01.css"/>
+                <link rel="stylesheet" id="leptos" href="/pkg/axum-leptos-tailwind-template.css"/>
                 <link rel="shortcut icon" type="image/ico" href="/favicon.ico"/>
                 <MetaTags/>
             </head>
